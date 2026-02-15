e6e8ddbd5bc0e651aabbcda23421cbd943260607
Dwi Ratna Sulistiyowati <dwiratnasulistiyowati@Dwis-MacBook-Air.local>
2026-02-15 16:26:08 +0700

    chore(backend): initial repository structure

---
+++ Dtos/RoomDtos.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace _2026_room_reservation_backend.Dtos;
+
+public class RoomDtos
+{
+
+}
---
+++ Models/Reservation.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace _2026_room_reservation_backend.Models;
+
+public class Reservation
+{
+
+}
